53c019708d5a3dde09d7d7e3ec2ab6d5dee62237
danilohmattos <danilohenrique2312@gmail.com>
2019-01-19 15:17:57 -0200

    Inserir e alterar

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);
 */
-
+//=========
 //carrega um usuario v
 
 /*
@@ -18,22 +18,37 @@ $root->loadbyId(3);
 
 echo $root;
 */
-
+//========
 // Carrega uma lista de usuários V
 /*
 $lista = Usuario::getList();
 
 echo json_encode($lista);
 */
-
+//========
 //Carrega uma lista de usuarios buscando pelo login
 //$search = Usuario::search("ro");
 //echo json_encode($search);
-
+//========
 //Carrega um usuario usando o login e senha
+//$usuario = new Usuario();
+//$usuario->login("danilo","12345667");
+
+//echo $usuario;
+/*
+//Criando um novo usuário INSERT
+$aluno = new Usuario("aluno", "@lun0");
+
+$aluno->insert();
+
+echo $aluno;
+*/
+
 $usuario = new Usuario();
-$usuario->login("danilo","12345667");
+
+$usuario->loadById(8);
+
+$usuario->update("professor", "a@%$¨#");
 
 echo $usuario;
-
  ?>
