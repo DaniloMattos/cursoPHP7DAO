e677010e73f45d7c55a18c0deb7d62a855aaeb03
danilohmattos <danilohenrique2312@gmail.com>
2019-01-19 11:47:55 -0200

    criado exemplo DAO

--- index.php
+++ index.php
@@ -9,11 +9,31 @@ $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 echo json_encode($usuarios);
 */
 
+//carrega um usuario v
+
+/*
 $root = new Usuario();
 
 $root->loadbyId(3);
 
 echo $root;
+*/
 
+// Carrega uma lista de usuários V
+/*
+$lista = Usuario::getList();
+
+echo json_encode($lista);
+*/
+
+//Carrega uma lista de usuarios buscando pelo login
+//$search = Usuario::search("ro");
+//echo json_encode($search);
+
+//Carrega um usuario usando o login e senha
+$usuario = new Usuario();
+$usuario->login("danilo","12345667");
+
+echo $usuario;
 
  ?>
